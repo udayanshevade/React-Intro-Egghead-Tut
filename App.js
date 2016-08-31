c41928ae9164a780048b63d983f81b76561c9542
Udayan Shevade <udayan.shevade@gmail.com>
2016-08-30 19:01:54 -0700

    Egghead Lesson #9 - Mounting Cycle

--- App.js
+++ App.js
@@ -2,60 +2,92 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  // Constructor for the component class
   constructor() {
+    // access to context
     super();
+    // define component properties
     this.state = {
-      red: 0,
-      green: 0,
-      blue: 0
+      level: 0
     };
     this.update = this.update.bind(this);
   }
-  update(e) {
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
-    });
+
+  // increment the level
+  update() {
+    this.setState({ level: this.state.level + 1 });
   }
+
+  // Mounting of the app component
+  // these are only relative to the local component
+  componentWillMount() {
+    console.log('App will unmount shortly.');
+  }
+  componentDidMount() {
+    console.log('App has successfully unmounted.');
+  }
+
+  // Unmounting the app component
+  // these are only relative to the local component
+  componentWillUnmount() {
+    console.log('App will unmount shortly.');
+  }
+  componentHasUnmounted() {
+    console.log('App has successfully unmounted.');
+  }
+
+  // default render function
   render() {
     return (
-      <main>
-
-        <Slider ref="red"
-          val={ this.state.red }
-          update={ this.update } />
-
-        <Slider ref="green"
-          val={ this.state.green }
-          update ={this.update } />
-
-        <Slider ref="blue"
-          val={ this.state.blue }
-           update= {this.update } />
-
-      </main>
+      <button onClick={this.update}>{ this.state.level }</button>
     );
   }
 };
 
-class Slider extends React.Component {
+class Wrapper extends React.Component {
+  constructor() {
+    super();
+    this.mount = this.mount.bind(this);
+    this.unmount = this.unmount.bind(this);
+  }
+
+  mount() {
+    console.log('Mounting the level.');
+    this.levelContainer = document.getElementById('level-container');
+    ReactDOM.render(<App/>, this.levelContainer);
+  }
+
+  componentWillMount() {
+    console.log('Wrapper will mount shortly.');
+  }
+  componentDidMount() {
+    console.log('Wrapper has successfully mounted.');
+  }
+
+  unmount() {
+    console.log('Unmounting the level.');
+    ReactDOM.unmountComponentAtNode(this.levelContainer);
+  }
+  componentWillUnmount() {
+    console.log('Wrapper will unmount shortly.');
+  }
+  componentHasUnmounted() {
+    console.log('Wrapper has successfully unmounted.');
+  }
+
   render() {
-    let val = this.props.val;
-    let update = this.props.update;
     return (
-      <label>
-        <div>{ val }</div>
-        <input ref="inp" type="range"
-          value={ val }
-          min="0" max="255"
-          onChange={update} />
-      </label>
+      <div>
+        <button onClick={this.mount}>Mount</button>
+        <button onClick={this.unmount}>Unmount</button>
+        <div id="level-container"></div>
+      </div>
     );
   }
+
 }
 
 ReactDOM.render(
-  <App cat={5}/>,
+  <Wrapper/>,
   document.getElementById('app')
 );
