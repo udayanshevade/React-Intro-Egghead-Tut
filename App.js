6a706c839754cb02b56418d28739809eaaa8b3ef
Udayan Shevade <udayan.shevade@gmail.com>
2016-09-18 11:37:30 -0700

    Create Basic JSX live compiler

--- App.js
+++ App.js
@@ -5,91 +5,44 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      red: 0
+      output: '',
+      input: '/* enter your jsx here */',
+      err: ''
     };
     this.update = this.update.bind(this);
   }
-  update() {
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value
-    });
+  update(e) {
+    let code = e.target.value;
+    try {
+      this.setState({
+        output: babel.transform(code, {
+          stage: 0,
+          loose: 'all'
+        }).code,
+        err: ''
+      });
+    } catch (err) {
+      this.setState({
+        err: err.message
+      });
+    }
   }
   render() {
     return (
       <main>
-        <NumInput ref="red"
-          min={0}
-          max={255}
-          step={1}
-          label="Value"
-          val={+this.state.red}
-          update={this.update} />
+        <header className="error-message">{this.state.err}</header>
+        <section className="container">
+          <textarea className="container-split input"
+            onChange={this.update}
+            defaultValue={this.state.input}></textarea>
+          <pre className="container-split output">
+            {this.state.output}
+          </pre>
+        </section>
       </main>
     );
   }
 }
-
-class NumInput extends React.Component {
-  constructor() {
-    super();
-    this.types = ['range', 'number'];
-    this.typeIndex = 0;
-    this.state = {
-      type: 'range'
-    };
-    this.toggle = this.toggle.bind(this);
-  }
-  toggle() {
-    this.props.update();
-    this.typeIndex = (this.typeIndex + 1) % 2;
-    this.setState({
-      type: this.types[this.typeIndex]
-    });
-  }
-  render() {
-    let label = this.props.label ? this.props.label + '-' : '';
-    let val = this.props.val;
-    let update = this.props.update;
-    return (
-      <div>
-        <label>
-          <div>{label} { val }</div>
-          <input
-            className="value-input"
-            ref="inp"
-            type={this.state.type}
-            defaultValue={val}
-            min={this.props.min}
-            max={this.props.max}
-            step={this.props.step}
-            onChange={update}/>
-        </label>
-        <label className="switch">
-          <input type="checkbox"
-            onChange={this.toggle}/>
-          <div className="slider round"></div>
-        </label>
-      </div>
-    );
-  }
-}
-
-NumInput.propTypes = {
-  min: React.PropTypes.number,
-  max: React.PropTypes.number,
-  val: React.PropTypes.number,
-  step: React.PropTypes.number,
-  label: React.PropTypes.string,
-  update: React.PropTypes.func.isRequired
-};
-
-NumInput.defaultProps = {
-  min: 0,
-  val: 0,
-  step: 1,
-  label: ''
-};
-
 
 ReactDOM.render(
   <App/>,
